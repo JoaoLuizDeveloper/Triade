5657f2be3a9105812bbf6a298841aff5d93afd9a
Juninho DRZ <juninhoworld2012@gmail.com>
2022-02-15 22:44:10 -0300

    First commit

---
+++ Triade/Models/Produtos.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Triade.Models
+{
+    public class Produtos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome do produto é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Informe o produto")]
+        [StringLength(150, MinimumLength = 4)]
+        public string NomeProduto { get; set; }
+
+        [Required(ErrorMessage = "O preço de custo do produto é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Informe o preço de custo do produto")]
+        public double PrecoCusto { get; set; }
+
+        [Required(ErrorMessage = "O preço de venda do produto é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Informe preço de venda do produto")]
+        public double PrecoVenda { get; set; }
+
+        public bool IsSimple { get; set; }
+        [Required(ErrorMessage = "A quantidade do produto é obrigatória", AllowEmptyStrings = false)]
+        [Display(Name = "Informe a quantidade do produto")]
+        public int Qtdproduto { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
---
+++ Triade/Models/Retirados.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Triade.Models
+{
+    public class Retirados
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "A quantidade retirada do produto é obrigatória", AllowEmptyStrings = false)]
+        [Display(Name = "Informe o quantidade do produto retirada")]
+        public int QtdRetirada { get; set; }
+
+        [Range(typeof(DateTime), "01/01/1900", "01/01/2023",
+        ErrorMessage = "Data de Retirada {0} Precisa ser entre {1} e {2}")]
+        [Display(Name = "Informe a Data de Retirada")]
+        public DateTime DataRetirada { get; set; }
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        public Produtos Produto { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
